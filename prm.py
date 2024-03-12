ffb355f8f448dc7a3049aaadba71e6f0a238f2c1
gao634 <gao634@purdue.edu>
2024-03-11 18:36:42 -0700

    wrote env class

--- prm.py
+++ prm.py
@@ -1,4 +1,8 @@
 import math
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
+import os
 
 # taken from assignment 1 part 2
 def diff(v1, v2):
@@ -10,7 +14,29 @@ def magnitude(v):
 def dist(p1, p2):
     return magnitude(diff(p1, p2))
 
-# this class creates the road map
+# this class loads an environment with obstacles to run PRM on
+class ENV():
+    def __init__(self, length=20, width=20, obs=[]):
+        self.length = length
+        self.width = width
+        self.obs=obs   
+    def load(self, path):
+        if path.endswith('.txt'):
+            self.obs = np.loadtxt(path, np.int32)
+        else:
+            self.obs = np.fromfile(path, np.int32)
+        self.length, self.width = self.obs.shape
+    # takes a plt as input so it can be called in visualizer.py
+    def visualize(self, plt):
+        # for now, our maze runs on grid coordinates and each obs is a 1x1 square
+        for x in self.obs:
+            for y in self.obs[0]:
+                if self.obs[x][y]:
+                    rect = mpatches.Rectangle((x, y), 1, 1, color='grey')
+                    plt.gca().add_patch(rect)
+        return plt
+
+# this class creates the road map. one rm is created per env to generate paths
 class PRM():
     def __init__():
         pass
@@ -27,12 +53,16 @@ class PRM():
     # saves the graph of the road map so it can be used later to generate paths
     def save():
         pass
+    # loads graph from data file for visuals
+    def load():
+        pass
+    def visualize():
+        pass
 
 # graph data structure
 class Graph():
     def __init__():
         pass
-    def load():
-        pass
-    def visualize():
-        pass
+
+env = ENV()
+env.load('./env_0.txt')
